52ac049c1ba9901189914877541e60354b4de292
Damien <damien.chapeau81@gmail.com>
2024-01-05 16:26:12 +0900

    generate Static pages and revalidation

--- src/actions/subway-api.tsx
+++ src/actions/subway-api.tsx
@@ -2,7 +2,10 @@ import { SubwayApiResponse } from '@/types/api-subway-info';
 
 export async function getSubwayInfo({ stationName }: { stationName: string }) {
   const response = await fetch(
-    `http://swopenAPI.seoul.go.kr/api/subway/6378464e4a776b643131375451674c43/json/realtimeStationArrival/0/4/${stationName}`
+    `http://swopenAPI.seoul.go.kr/api/subway/6378464e4a776b643131375451674c43/json/realtimeStationArrival/0/4/${stationName}`,
+    {
+      next: { revalidate: 10 },
+    }
   );
   const result: SubwayApiResponse = await response.json();
   return result;
--- src/app/[lineId]/layout.tsx
+++ src/app/[lineId]/layout.tsx
@@ -1,6 +1,14 @@
 import { ComboboxSubwayStations } from '@/components/select-subway';
 import { serverClient } from '../_trpc/serverClient';
 
+// This function will be called at build time to generate the list of paths
+export async function generateStaticParams() {
+  const lines = await serverClient.getLines();
+  return lines.map((line) => ({
+    lineId: line.lineId.toString(),
+  }));
+}
+
 export default async function Layout({
   children,
   params,
