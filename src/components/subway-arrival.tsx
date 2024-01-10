0260b7ab3ec700d116050ca7fc28eea2e6ba0bf5
Damien <damien.chapeau81@gmail.com>
2024-01-10 14:45:46 +0900

    Train position depending on API message

--- src/components/subway-arrival.tsx
+++ src/components/subway-arrival.tsx
@@ -7,8 +7,32 @@ const secondsToMinutesFormat = (seconds: number) => {
   return `${minutes}min${secondsLeft}`;
 };
 
+const getTrainLocation = ({
+  arvlMsg2,
+  statnNm,
+}: Partial<RealtimeArrivalItem>) => {
+  switch (arvlMsg2) {
+    case `${statnNm} 진입`:
+      return 'right-10';
+    case `${statnNm} 도착`:
+      return 'right-5';
+    case statnNm:
+      return 'right-5';
+    case `${statnNm} 출발`:
+      return 'right-0';
+    case '전역 진입':
+      return 'left-0';
+    case '전역 도착':
+      return 'left-5';
+    case '전역 출발':
+      return 'left-10';
+    default:
+      return '';
+  }
+};
+
 const SubwayView = ({ data }: { data: RealtimeArrivalItem }) => {
-  const showTrainLocation = data.statnFNm === data.arvlMsg3;
+  const trainLocation = getTrainLocation(data);
   return (
     <>
       <div className="flex justify-between items-center p-6 gap-6 relative">
@@ -20,8 +44,11 @@ const SubwayView = ({ data }: { data: RealtimeArrivalItem }) => {
           <Circle className="h-4 w-4 fill-black" />
           <span>{data.statnNm}</span>
         </div>
-        {showTrainLocation && (
-          <div className="absolute left-0 -top-4" id="trainLocation">
+        {trainLocation !== '' && (
+          <div
+            id="trainLocation"
+            className={`absolute -top-4 ${trainLocation}`}
+          >
             <div className="relative h-12 w-12">
               <RectangleHorizontal className="absolute h-12 w-12 stroke-lime-500" />
               <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-sm font-bold">
